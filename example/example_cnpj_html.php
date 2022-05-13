cccae4ff05997ca76476d7a39707ecb39d639d11
Paulo Silva <paulocdjsilva@gmail.com>
2022-05-12 21:39:58 -0300

    Atualização dos exemplos HTML

--- example/example_cnpj_html.php
+++ example/example_cnpj_html.php
@@ -5,6 +5,28 @@ Use Src\Cnpj;
 
 echo "<br><br>";
 echo "==========================================================<br>";
+echo "Generating formatted CNPJ<br>";
+echo "==========================================================<br>";
+$cnpj = Cnpj::generate();
+
+var_dump($cnpj);
+echo "<br>";
+var_dump(Cnpj::validate($cnpj));
+
+echo "<br><br>";
+echo "==========================================================<br>";
+echo "Generating unformatted CNPJ<br>";
+echo "==========================================================<br>";
+$cnpj = Cnpj::generate(false);
+
+var_dump($cnpj);
+echo "<br>";
+var_dump(Cnpj::formate($cnpj));
+echo "<br>";
+var_dump(Cnpj::validate($cnpj));
+
+echo "<br><br>";
+echo "==========================================================<br>";
 echo "CNPJ 64.123.337/0001-79<br>";
 echo "==========================================================<br>";
 var_dump(Cnpj::validate('64.123.337/0001-79'));
--- example/example_cpf_html.php
+++ example/example_cpf_html.php
@@ -5,6 +5,28 @@ Use Src\Cpf;
 
 echo "<br><br>";
 echo "==========================================================<br>";
+echo "Generating formatted CPF<br>";
+echo "==========================================================<br>";
+$cpf = Cpf::generate();
+
+var_dump($cpf);
+echo "<br>";
+var_dump(cpf::validate($cpf));
+
+echo "<br><br>";
+echo "==========================================================<br>";
+echo "Generating unformatted CPF<br>";
+echo "==========================================================<br>";
+$cpf = Cpf::generate(false);
+
+var_dump($cpf);
+echo "<br>";
+var_dump(cpf::formate($cpf));
+echo "<br>";
+var_dump(cpf::validate($cpf));
+
+echo "<br><br>";
+echo "==========================================================<br>";
 echo "CPF 790.055.670-23<br>";
 echo "==========================================================<br>";
 var_dump(Cpf::validate('790.055.670-23'));
