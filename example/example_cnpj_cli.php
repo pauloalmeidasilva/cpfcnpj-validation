b11ca4d3ac2e6004adee25793f392616c4745686
Paulo Silva <paulocdjsilva@gmail.com>
2022-05-12 21:36:23 -0300

    Atualização dos exemplos CLI

--- example/example_cnpj_cli.php
+++ example/example_cnpj_cli.php
@@ -5,6 +5,25 @@ Use Src\Cnpj;
 
 echo "\n\n";
 echo "==========================================================\n";
+echo "Generating formatted CNPJ\n";
+echo "==========================================================\n";
+$cnpj = Cnpj::generate();
+
+var_dump($cnpj);
+var_dump(Cnpj::validate($cnpj));
+
+echo "\n\n";
+echo "==========================================================\n";
+echo "Generating unformatted CNPJ\n";
+echo "==========================================================\n";
+$cnpj = Cnpj::generate(false);
+
+var_dump($cnpj);
+var_dump(Cnpj::formate($cnpj));
+var_dump(Cnpj::validate($cnpj));
+
+echo "\n\n";
+echo "==========================================================\n";
 echo "CNPJ 64.123.337/0001-79\n";
 echo "==========================================================\n";
 var_dump(Cnpj::validate('64.123.337/0001-79'));
--- example/example_cpf_cli.php
+++ example/example_cpf_cli.php
@@ -5,6 +5,25 @@ Use Src\Cpf;
 
 echo "\n\n";
 echo "==========================================================\n";
+echo "Generating formatted CPF\n";
+echo "==========================================================\n";
+$cpf = Cpf::generate();
+
+var_dump($cpf);
+var_dump(cpf::validate($cpf));
+
+echo "\n\n";
+echo "==========================================================\n";
+echo "Generating unformatted CPF\n";
+echo "==========================================================\n";
+$cpf = Cpf::generate(false);
+
+var_dump($cpf);
+var_dump(cpf::formate($cpf));
+var_dump(cpf::validate($cpf));
+
+echo "\n\n";
+echo "==========================================================\n";
 echo "CPF 790.055.670-23\n";
 echo "==========================================================\n";
 var_dump(Cpf::validate('790.055.670-23'));
